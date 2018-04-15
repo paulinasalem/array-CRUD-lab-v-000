12bb3b58ba173903b02f1b2942015b8c3d45597b
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 16:43:54 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+array = ["Becky", "Ruby", "am", "Joelle", "Marielle"]
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
