04f16f2e7db821d0b55c2c25749248cd3dbda8a5
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 16:42:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
 array = ["Becky", "Ruby", "Joelle", "Marielle", "arrays!"]
-"arrays!" = array.pop
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
